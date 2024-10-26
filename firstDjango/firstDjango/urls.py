410baba7f7fe37ad4b24038f1b148baa1b9e4d55
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-26 13:59:56 +0530

    add app to the django project

--- firstDjango/firstDjango/urls.py
+++ firstDjango/firstDjango/urls.py
@@ -15,15 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home, name="home"),
-    path('about', views.about, name="home"),
-    path('contact', views.contact, name="home"),
-    path('name', views.name, name="home")
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+    path('name', views.name, name="name"),
+    path('firstapp/', include('firstapp.urls'))
     
 ]
+
+
 
