5d4bec76323ea9f07f129cd4b36b431cfc6d5051
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-30 18:15:05 +0530

    learn about relation one to one, one to many and many to many

--- firstDjango/firstapp/models.py
+++ firstDjango/firstapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class User(models.Model):
@@ -13,9 +14,41 @@ class User(models.Model):
     description = models.TextField(max_length=500, blank=True,null=True)
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=8, choices=USER_TYPE)
+    
+    def __str__(self):
+        return self.name
+    
 
+# one to many
+class UserReviw(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    getUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.getUser.username} review for {self.user.name}'
+        
+# many to many relation
+
+class Store(models.Model):
+    name= models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    userType = models.ManyToManyField(User, related_name='store')
     
     
     def __str__(self):
         return self.name
+    
+# one to one relation
+
+class UserCertificate(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+    
+    def __str__(self):
+        return f'Certificate for {self.user.name}'
     
