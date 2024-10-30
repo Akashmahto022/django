1f5da4f4c1e8851af715f11d99798455c04e3fb9
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-30 13:47:34 +0530

    add email and description to user model

--- firstDjango/firstapp/models.py
+++ firstDjango/firstapp/models.py
@@ -9,11 +9,13 @@ class User(models.Model):
     ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
+    email = models.EmailField(max_length=120, blank=False, null=False, default='')
+    description = models.TextField(max_length=500, blank=True,null=True)
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=8, choices=USER_TYPE)
+
     
     
     def __str__(self):
         return self.name
-    
     
