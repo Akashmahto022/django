0a9343614fa7083292e737713c19ec80d01b9d9f
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-30 14:05:29 +0530

    created on more page for user details and created url and views

--- firstDjango/firstapp/urls.py
+++ firstDjango/firstapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.firstapp, name="firstapp"),
+    path('<int:user_id>',views.user_details, name="user-details" )
     
 ]
--- firstDjango/firstapp/views.py
+++ firstDjango/firstapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import User
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -7,3 +8,7 @@ from .models import User
 def firstapp(request):
     users = User.objects.all()
     return render(request, 'firstapp/first.html', {'users': users})
+
+def user_details(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request,'firstapp/user_details.html', {"user":user})
